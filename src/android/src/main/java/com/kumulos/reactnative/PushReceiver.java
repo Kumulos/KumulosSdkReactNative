15035d89553b89b63d50f8034e686a356fbb6739
vvoicehovics <vladislav.voicehovich@gmail.com>
2020-02-03 10:33:00 +0000

    add button clicked action to manifest, launch activity when clicked

--- src/android/src/main/java/com/kumulos/reactnative/PushReceiver.java
+++ src/android/src/main/java/com/kumulos/reactnative/PushReceiver.java
@@ -3,6 +3,10 @@ package com.kumulos.reactnative;
 import android.content.Context;
 import android.net.Uri;
 import android.content.Intent;
+import android.content.ComponentName;
+import android.app.Activity;
+import android.os.Build;
+import android.app.TaskStackBuilder;
 
 import com.facebook.react.bridge.WritableMap;
 import com.facebook.react.bridge.WritableNativeMap;
@@ -65,6 +69,11 @@ public class PushReceiver extends PushBroadcastReceiver {
     static class PushActionHandler implements PushActionHandlerInterface {
         @Override
         public void handle(Context context, PushMessage pushMessage, String actionId) {
+            Intent it = new Intent(Intent.ACTION_CLOSE_SYSTEM_DIALOGS);
+            context.sendBroadcast(it);
+
+            this.launchActivity(context, pushMessage);
+
             if (null == KumulosReactNative.sharedReactContext) {
                 KumulosReactNative.coldStartPush = pushMessage;
                 KumulosReactNative.coldStartActionId = actionId;
@@ -73,9 +82,44 @@ public class PushReceiver extends PushBroadcastReceiver {
 
             KumulosReactNative.sharedReactContext.getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
                 .emit("kumulos.push.opened", pushToMap(pushMessage, actionId));
+        }
 
-            Intent it = new Intent(Intent.ACTION_CLOSE_SYSTEM_DIALOGS);
-            context.sendBroadcast(it);
+        private void launchActivity(Context context, PushMessage pushMessage){
+            PushReceiver pr = new PushReceiver();
+            Intent launchIntent = pr.getPushOpenActivityIntent(context, pushMessage);
+
+            if (null == launchIntent) {
+                return;
+            }
+            ComponentName component = launchIntent.getComponent();
+            if (null == component) {
+                return;
+            }
+            Class<? extends Activity> cls = null;
+            try {
+                cls = (Class<? extends Activity>) Class.forName(component.getClassName());
+            } catch (ClassNotFoundException e) {
+                /* Noop */
+            }
+
+            if (null == cls) {
+                return;
+            }
+
+            addDeepLinkExtras(pushMessage, launchIntent);
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+                TaskStackBuilder taskStackBuilder = TaskStackBuilder.create(context);
+                taskStackBuilder.addParentStack(component);
+                taskStackBuilder.addNextIntent(launchIntent);
+
+                taskStackBuilder.startActivities();
+                return;
+            }
+
+            launchIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+
+            context.startActivity(launchIntent);
         }
     }
 }
