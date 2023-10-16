ac5a6a3e3bba93bd21c4253592b7a4677e6a8b9b
Christopher Wyllie <chris_w@optimove.com>
2023-10-16 11:52:17 +0100

    Bump version

--- src/ios/KumulosReactNative.m
+++ src/ios/KumulosReactNative.m
@@ -12,7 +12,7 @@ API_AVAILABLE(ios(10.0))
 static KSPushReceivedInForegroundHandlerBlock ksPushReceivedHandler;
 static KSPushNotification* _Nullable ksColdStartPush;
 
-static const NSString* KSReactNativeVersion = @"7.0.0";
+static const NSString* KSReactNativeVersion = @"8.0.0";
 static const NSUInteger KSSdkTypeReactNative = 9;
 static const NSUInteger KSRuntimeTypeReactNative = 7;
 
